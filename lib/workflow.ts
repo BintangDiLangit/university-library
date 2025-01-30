2e1de26b2ef1641271b0be46e5416be3567b5696
BintangDiLangit <supercellbanned1@gmail.com>
2025-01-30 07:44:54 +0700

    update email onborading

--- lib/workflow.ts
+++ lib/workflow.ts
@@ -26,7 +26,7 @@ export const sendEmail = async ({
       provider: resend({ token: config.env.resendToken }),
     },
     body: {
-      from: "BINTANGMFHD <hello.bintangmfhd.com>",
+      from: "BINTANGMFHD <contact.bintangmfhd.com>",
       to: [email],
       subject,
       html: message,
